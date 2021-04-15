b05e5a86b70f1ca5bb92963746d716fbd7028641
Steffen <steffen1.koehler@web.de>
2021-04-15 19:42:30 +0200

    safety commit

--- src/evaluator.c
+++ src/evaluator.c
@@ -2,27 +2,33 @@
 
 scmObject scm_eval(scmObject inputUnevaluated)
 {
-    // scmObject car;
-    // scmObject restList;
-    // scmObject nextArg;
-    // int sum;
+    scmObject car;
+    scmObject restList;
+    scmObject nextArg;
 
-    // if (inputUnevaluated->tag == TAG_CONS)
-    // {
-    //     car = inputUnevaluated->value.scmCons.car;
-    //     restList = inputUnevaluated->value.scmCons.cdr;
+    if (inputUnevaluated->tag == TAG_CONS)
+    {
 
-    //     // Wenn es eine Cons ist muss man was machen
-    //     if (car == newSymbol("+", 1))
-    //     {
-    //         while (restList->tag != TAG_NULL)
-    //         {
-    //             sum += restList.
-    //         }
-    //     }
-    // }
+        car = inputUnevaluated->value.scmCons.car;
+        restList = inputUnevaluated->value.scmCons.cdr;
 
+        printf("scm_eval:------------------------- \n");
+        scm_print(restList);
+        printf("\n-----------------\n");
+        // Wenn es eine Cons ist muss man was machen
+        if (car == newSymbol("+", 1))
+        {
+            printf("Betrete if\n");
+            int sum;
+            while (restList->tag != TAG_NULL)
+            {
+                nextArg = restList->value.scmCons.car;
+                sum += nextArg->value.scmInt;
+                restList = restList->value.scmCons.cdr;
+            }
+            return (newInteger(sum));
+        }
+    }
     // Der ganze Rest evaluiert einfach zu sich selbst (Int, True, ...)
-
     return inputUnevaluated;
 }
